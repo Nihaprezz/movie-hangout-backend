5fbe0fbdc8bcb61693edc18a2cbb15591ae0c903
nihaprezz <sergio13prez@gmail.com>
2019-12-01 21:14:28 -0500

    finished the basic user login routes

--- Gemfile
+++ Gemfile
@@ -45,3 +45,5 @@ gem "jwt", "~> 2.2"
 gem "active_model_serializers", "~> 0.10.10"
 
 gem "faker", "~> 2.8"
+
+gem 'figaro'
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :username, :avatar, :bio
+  #may need to come back and add the additional field to this 
+  #attributes :username, :avatar, :bio, :location, :favorite_genre
 end
