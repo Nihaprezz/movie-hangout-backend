a142fd090f9876a92c45f4f5b157cd964a504be0
nihaprezz <sergio13prez@gmail.com>
2019-12-01 22:58:33 -0500

    comments for seeding data

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,14 @@
 class Movie < ApplicationRecord
     has_many :genre_joints
     has_many :genres, through: :genre_joints
+
+    #need to seed the genre tables with the API url, only 19 genres
+    #need to seed the movie using the popular API url. 
+    #create a new genre joint table relation ship for every genre a movie has. 
+    #probably a couple of .each enumarable methods when pulling the info from the API
+    # a movie can have one or two genres. 
+    # need to create a serialized route for the right info of information.
+
+    def seed_data
+    end
 end
